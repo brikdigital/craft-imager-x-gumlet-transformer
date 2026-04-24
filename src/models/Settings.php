4f7018bfdf437879e5b2a9ae759d267310b6282e
Alyxia Sother <alyxia@riseup.net>
2026-04-24 13:14:32 +0200

    Support custom domains & signed URLs

--- src/models/Settings.php
+++ src/models/Settings.php
@@ -7,9 +7,18 @@ use craft\base\Model;
 class Settings extends Model
 {
     public string $subdomain = '';
+    public string $customDomain = '';
     public string $apiKey = '';
+    public string $signingKey = '';
     public string $defaultProfile = '';
     public array $profiles = [];
     public bool $enableCompression = true;
     public bool $hookCpImages = false;
+
+    protected function defineRules(): array
+    {
+        return [
+            ['customDomain', 'match', 'not' => true, 'pattern' => '/:\/\//']
+        ];
+    }
 }
