6e4abd5d9c0e1ba5fc9e43e95aecac99757c0217
Bram Nijssen <bram@brik.digital>
2026-07-17 18:06:38 +0200

    Quality settings based on file extension, remove unsupported transform properties

--- src/models/GumletTransformedImageModel.php
+++ src/models/GumletTransformedImageModel.php
@@ -5,6 +5,7 @@ namespace brikdigital\gumlettransformer\models;
 use craft\elements\Asset;
 use spacecatninja\imagerx\models\BaseTransformedImageModel;
 use spacecatninja\imagerx\models\TransformedImageInterface;
+use spacecatninja\imagerx\services\ImagerService;
 
 class GumletTransformedImageModel extends BaseTransformedImageModel implements TransformedImageInterface
 {
@@ -23,6 +24,28 @@ class GumletTransformedImageModel extends BaseTransformedImageModel implements T
         }
 
         if ($transform !== []) {
+            // Set quality
+            if (!isset($transform['quality'])) {
+                $ext = null;
+
+                if ($source) {
+                    $ext = $source->getExtension();
+                } else if ($imageUrl) {
+                    $pathParts = pathinfo($source);
+                    $ext = $pathParts['extension'] ?? '';
+                }
+
+                $transform['quality'] = $this->getQualityFromExtension($ext, $transform);
+            }
+
+            // unset unsupported properties
+            unset(
+                $transform['jpegQuality'],
+                $transform['pngCompressionLevel'],
+                $transform['webpQuality'],
+                $transform['fillInterval'],
+            );
+
             $this->transform = $transform;
             $this->width = $source->getWidth($transform);
             $this->height = $source->getHeight($transform);
@@ -37,5 +60,19 @@ class GumletTransformedImageModel extends BaseTransformedImageModel implements T
     public function getHeight(): int
     {
         return !!$this->source ? $this->source->getHeight($this->transform) : 0;
+    }
+
+    private function getQualityFromExtension(string $ext, array $transform = null): string
+    {
+        $config = ImagerService::getConfig();
+
+        if ($ext == 'png') {
+            $pngCompression = $config->getSetting('pngCompressionLevel', $transform);
+            return max(100 - ($pngCompression * 10), 1);
+        } elseif ($ext == 'webp') {
+            return $config->getSetting('webpQuality', $transform);
+        }
+
+        return $config->getSetting('jpegQuality', $transform);
     }
 }
